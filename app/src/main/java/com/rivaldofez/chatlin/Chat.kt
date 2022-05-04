432a9421670286330504050a18d0959e81bbfb8a
rivaldofez <rivaldofez@gmail.com>
2022-05-04 21:30:26 +0700

    finish build the main feature

--- app/src/main/java/com/rivaldofez/chatlin/Chat.kt
+++ app/src/main/java/com/rivaldofez/chatlin/Chat.kt
@@ -3,10 +3,20 @@ package com.rivaldofez.chatlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.rivaldofez.chatlin.databinding.ActivityChatBinding
+import com.rivaldofez.chatlin.model.Message
 
 class Chat : AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var mDbRef: DatabaseReference
+
+    var receiverRoom: String? = null
+    var senderRoom: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +24,51 @@ class Chat : AppCompatActivity() {
         setContentView(binding.root)
 
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+
+        mDbRef = FirebaseDatabase.getInstance("https://chatlin-34a9a-default-rtdb.asia-southeast1.firebasedatabase.app").getReference()
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
 
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+
+        binding.messageRecyclerview.layoutManager = LinearLayoutManager(this)
+        binding.messageRecyclerview.adapter = messageAdapter
+
+        //logic for add data to recylerview
+        mDbRef.child("chats").child(senderRoom!!).child("messages")
+            .addValueEventListener(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+                    for(postSnapshot in snapshot.children){
+                        val message = postSnapshot.getValue(Message::class.java)
+                        messageList.add(message!!)
+                    }
+                    messageAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
+
+        //adding message to database
+        binding.btnSend.setOnClickListener {
+            val message = binding.edtMessage.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+
+            binding.edtMessage.setText("")
+        }
     }
 }
