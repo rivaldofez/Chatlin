0713d69bb2050e645603c9b0daaabb3a69f2e924
rivaldofez <rivaldofez@gmail.com>
2022-05-04 16:50:08 +0700

    add view binding and set login as main activity

--- app/src/main/java/com/rivaldofez/chatlin/Login.kt
+++ app/src/main/java/com/rivaldofez/chatlin/Login.kt
@@ -1,11 +1,21 @@
 package com.rivaldofez.chatlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.chatlin.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
