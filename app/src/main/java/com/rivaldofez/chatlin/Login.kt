465e206ff426bd78cb8a2e2b2387328c61fc9b4d
rivaldofez <rivaldofez@gmail.com>
2022-05-04 17:29:55 +0700

    create base firebase auth and setup sdk

--- app/src/main/java/com/rivaldofez/chatlin/Login.kt
+++ app/src/main/java/com/rivaldofez/chatlin/Login.kt
@@ -3,19 +3,35 @@ package com.rivaldofez.chatlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.rivaldofez.chatlin.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+
         binding.btnSignup.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        
     }
 }
--- app/src/main/java/com/rivaldofez/chatlin/SignUp.kt
+++ app/src/main/java/com/rivaldofez/chatlin/SignUp.kt
@@ -2,10 +2,29 @@ package com.rivaldofez.chatlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.rivaldofez.chatlin.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignup.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            signUp(email, password)
+        }
+    }
+
+    private fun signUp(email: String, password: String) {
+
     }
 }
