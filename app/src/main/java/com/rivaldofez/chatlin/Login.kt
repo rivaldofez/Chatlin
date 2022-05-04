a93be41029c5ac9279fb65fc3b625cdaf2a366fd
rivaldofez <rivaldofez@gmail.com>
2022-05-04 17:38:59 +0700

    add function auth for login page

--- app/src/main/java/com/rivaldofez/chatlin/Login.kt
+++ app/src/main/java/com/rivaldofez/chatlin/Login.kt
@@ -3,6 +3,7 @@ package com.rivaldofez.chatlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.rivaldofez.chatlin.databinding.ActivityLoginBinding
 
@@ -32,6 +33,14 @@ class Login : AppCompatActivity() {
     }
 
     private fun login(email: String, password: String) {
-        
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val intent = Intent(this, Home::class.java )
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "User does not exist, sign up first", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
