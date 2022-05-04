e1e49191905cbf8bf105fd854dd3c47b389e3c1c
rivaldofez <rivaldofez@gmail.com>
2022-05-04 19:07:36 +0700

    add firebase realtime database and function to save user on database

--- app/src/main/java/com/rivaldofez/chatlin/Home.kt
+++ app/src/main/java/com/rivaldofez/chatlin/Home.kt
@@ -2,10 +2,20 @@ package com.rivaldofez.chatlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.chatlin.databinding.ActivityHomeBinding
+import com.rivaldofez.chatlin.model.User
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
     }
 }
--- app/src/main/java/com/rivaldofez/chatlin/SignUp.kt
+++ app/src/main/java/com/rivaldofez/chatlin/SignUp.kt
@@ -3,13 +3,18 @@ package com.rivaldofez.chatlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.rivaldofez.chatlin.databinding.ActivitySignUpBinding
+import com.rivaldofez.chatlin.model.User
 
 class SignUp : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +26,31 @@ class SignUp : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
 
         binding.btnSignup.setOnClickListener {
+            val name = binding.edtName.text.toString()
             val email = binding.edtEmail.text.toString()
             val password = binding.edtPassword.text.toString()
 
-            signUp(email, password)
+            signUp(name, email, password)
         }
     }
 
-    private fun signUp(email: String, password: String) {
+    private fun signUp(name: String, email: String, password: String) {
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if(task.isSuccessful){
+                    mAuth.currentUser?.let { addUserToDatabase(name, email, it.uid) }
                     val intent = Intent(this, Home::class.java )
                     startActivity(intent)
                 } else {
                     Toast.makeText(this, "Some error occured", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String) {
+
+        mDbRef = FirebaseDatabase.getInstance("https://chatlin-34a9a-default-rtdb.asia-southeast1.firebasedatabase.app").getReference()
+        mDbRef.child("user").child(uid).setValue(User(name, email, uid))
+        Log.d("Testing", "called addUserToDatabase")
     }
 }
